e6497d7d1101999b74852fbfe30d13e4b9df6725
tomysnatalia <hanf.natalia@gmail.com>
2019-12-04 14:08:54 +0100

    First Commit

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+    public static void someMethod() {
+        System.out.println("It works!");
+    }
+
+    public static void main (String [] args) {
+        Calculator calculator = new Calculator();
+        calculator.someMethod();
+    }
+}
